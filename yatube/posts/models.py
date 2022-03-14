5affcbb8dcffdb8f42f48a11fa4d295a8a0130bc
Бедненко Александр <bednenko1604@gmail.com>
2022-03-14 07:29:06 +0300

    Finish

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 User = get_user_model()
+Group = 'Group'
 
 
 class Post(models.Model):
@@ -12,7 +13,7 @@ class Post(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True,
                                     verbose_name='Date of publication',)
     group = models.ForeignKey(
-        'Group',
+        Group,
         related_name='posts',
         blank=True,
         null=True,
